0b21f254f1e79bf0fdcfded178ce416b48cb75bc
Guan Yilun <zoom.aaron@gmail.com>
2013-05-23 11:21:10 -0700

    [update]

--- cal.py
+++ cal.py
@@ -1,5 +1,19 @@
-'''
-Created on May 17, 2013
+import const
+import numpy as np
+import scipy.integrate as spint
 
-@author: dave
-'''
+def ta(freq, intensity):
+    f = np.array(freq)
+    i = np.array(intensity)
+    ta = i*(const.c)**2/(const.k*(f**2))
+    return ta
+
+def bling(freq, intensity, resol):
+    f = np.array(freq)
+    t = ta(freq, intensity)
+    integral = const.h * const.k * f * t
+    result = spint.simps(integral, freq)
+    return result
+
+def trancate(freq, start, end):
+    
---
+++ const.py
@@ -0,0 +1,12 @@
+'''
+Created on May 22, 2013
+
+@author: dave
+'''
+
+'Physical Constants'
+
+h = 6.63*10**(-34)
+k = 1.38*10**(-23)
+c = 3*10**8
+
