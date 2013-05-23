13bf72cc9a331772a580dd832d9043eaeb244c42
loisshen <loisshendream@gmail.com>
2013-05-23 13:58:29 -0600

    Update const.py
    commit

--- const.py
+++ const.py
@@ -6,7 +6,14 @@ Created on May 22, 2013
 
 'Physical Constants'
 
-h = 6.63*10**(-34)
-k = 1.38*10**(-23)
-c = 3*10**8
+#general constants
+h = 6.63*10**(-34) # J*s
+k = 1.38*10**(-23) # J/K
+c = 3*10**8 # m/s
+
+#material constants for Thermal Mirror Emission
+sigma_Be=2.500*10**7 # Ω**(-1)*m**(-1)
+sigma_Al=3.538*10**7 # Ω**(-1)*m**(-1)
+sigma_Au=4.060*10**7 # Ω**(-1)*m**(-1)
+sigma_Ag=6.287*10**7 # Ω**(-1)*m**(-1)
 
