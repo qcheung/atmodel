aae334fa88a5bc1a153c17209e34dc228fff5d27
loisshen <loisshendream@gmail.com>
2013-05-28 12:09:34 -0600

    Update cal.py

--- cal.py
+++ cal.py
@@ -3,38 +3,6 @@ import numpy as np
 import scipy.integrate as integrate
 import plotter
 
-def ta(freq, intensity):
-    f = np.array(freq)
-    i = np.array(intensity)
-    ta = i*(const.c)**2/(const.k*(f**2))
-    return ta
-
-def bling(freq, intensity, resol):
-    result = []
-    for v0 in freq:
-        freq_begin = v0 - v0/resol
-        freq_end = v0 + v0/resol
-        data = [freq,intensity]
-        data = trancate(data, freq_begin, freq_end)
-        f = data[0]
-        i = data[1]
-        t = ta(f,i)
-        integral = const.h * const.k * f * i * t
-        result.append(integrate.simps(integral, i)**(0.5))
-    return result
-    
-def bling_by_temperature(freq, temp, resol):
-    result = []
-    data = [freq,temp]
-    for v0 in freq:
-        freq_begin = v0 - v0/float(resol)
-        freq_end = v0 + v0/float(resol)
-        temp_data = trancate(data, freq_begin, freq_end)
-        temp_freq = temp_data[0]
-        temp_temp = temp_data[1]
-        integral = const.h * const.k * temp_freq * temp_temp
-        result.append(integrate.trapz(integral, temp_freq)**(0.5))
-    return result
 
 #bling_Cosmology_Infrared_Backgrond   
 def bling_CIB(freq, temp, resol):
@@ -77,7 +45,14 @@ def bling_TME(freq, temp, resol):
     return result
     
 #bing_Atmospheric_Radiance
-
+def bling_AR(freq, rad, resol):
+    result = []
+    for i in range(len(freq)):
+        v0 = freq[i]
+        i0 = rad[i]
+        t0 = i0 * const.c**2 / (const.k * v0**2)
+        result.append(const.h * const.k * v0 * t0 * 2 * v0 / resol)
+    return result    
 
 #bling_Zodiacal_Emission
 def bling_ZE(freq, temp, resol):
@@ -88,17 +63,38 @@ def bling_ZE(freq, temp, resol):
         result.append(const.h * const.k * v0 * t0 * 2 * v0 / resol)
     return result
 
-#bling_TOT
-   
-#Signal_Intensity
-def SI(freq, 
 #Total_Signal
+def TS(freq, rad, tao, d):
+    result = []
+    for i in range(len(freq)):
+        v0 = freq[i]
+        i0 = rad[i]
+        p0 = np.pi * (d / 2)**2 * tao * i0 * 2 * v0 / resol
+        result.append(p0)
+    return result
 
 #Limiting_Flux
+def LF(freq, rad, tao, d, resol):
+    result = []
+    for i in range(len(freq)):
+        v0 = freq[i]
+        i0 = rad[i]
+        p0 = TS(freq, rad, tao, d)
+        result.append((p0 *resol) / (2 * np.pi * (d / 2)**2 * v0))
+    return result
 
 #Integration_Time
+def IT(freq, bling, ratio, rad, tao, d):
+    result = []
+    for i in range(len(freq)):
+        v0 = freq[i]
+        n0 = bling[i]
+        p0 = TS(freq, rad, tao, d)
+        result.append((n0 * ratio / p0)**2)
+    return result
 
-    
+
+
 def trancate(data, start, end):
     '''
     Used to trancate an array based on frequency
@@ -112,7 +108,41 @@ def trancate(data, start, end):
             result.append(x)
     print np.transpose(result)
     return np.transpose(result)
+
 #testing 
 x = range(100)
 y = bling_by_temperature(x,range(100),5)
 plotter.loglogplot(x, y)
+
+def ta(freq, intensity):
+    f = np.array(freq)
+    i = np.array(intensity)
+    ta = i*(const.c)**2/(const.k*(f**2))
+    return ta
+
+def bling(freq, intensity, resol):
+    result = []
+    for v0 in freq:
+        freq_begin = v0 - v0/resol
+        freq_end = v0 + v0/resol
+        data = [freq,intensity]
+        data = trancate(data, freq_begin, freq_end)
+        f = data[0]
+        i = data[1]
+        t = ta(f,i)
+        integral = const.h * const.k * f * i * t
+        result.append(integrate.simps(integral, i)**(0.5))
+    return result
+    
+def bling_by_temperature(freq, temp, resol):
+    result = []
+    data = [freq,temp]
+    for v0 in freq:
+        freq_begin = v0 - v0/float(resol)
+        freq_end = v0 + v0/float(resol)
+        temp_data = trancate(data, freq_begin, freq_end)
+        temp_freq = temp_data[0]
+        temp_temp = temp_data[1]
+        integral = const.h * const.k * temp_freq * temp_temp
+        result.append(integrate.trapz(integral, temp_freq)**(0.5))
+    return result
