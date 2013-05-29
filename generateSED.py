cab29e6ae1f5b40cacc865a244b7990f09d1138e
Guan Yilun <zoom.aaron@gmail.com>
2013-05-29 16:57:36 -0700

    update

--- generateSED.py
+++ generateSED.py
@@ -3,7 +3,7 @@ import numpy as np
 import excel
 import cal
 import plotter
-
+'''
 #constant_NGC_958
 a_radio = -0.200
 Td = 26.9 #k
@@ -20,31 +20,31 @@ beta = 1.21
 a_high = -1.56
 v_radio = 0.24*10**12 #Hz
 v_midIR =  4.8*10**12 #Hz
-'''
+c = [0.877, 0.991, 0.959]
 
 #generate SED
 def SED(freq):
     result = []
     for i in range(len(freq)):
         v0 = freq[i]
-        if v0 <= v_radio:
-            f = v0**a_radio
-        elif v0 <= v_midIR:
-            f = v0**beta * (2 * const.h * v0**3 / const.c**2) * ((np.exp(const.h * v0 / (const.k * Td))-1)**(-1))
+        if v0 < v_radio:
+            f = c[0]*v0**a_radio
+        elif v0 < v_midIR:
+            f = c[1]*v0**beta * (2 * const.h * v0**3 / const.c**2) * ((np.exp(const.h * v0 / (const.k * Td))-1)**(-1))
         else:
-            f = v0**a_high
+            f = c[2]*v0**a_high
         result.append(f)
     return result
         
-xw = excel.ExcelWriter("/home/dave/SED_generate.xlsx")
-freq_cm = np.array(cal.generate_freq())
-xw.write_col("freq/cm^-1", freq_cm)
+#xw = excel.ExcelWriter("/home/dave/SED_generate.xlsx")
+freq_cm = np.array(cal.generate_freq(start = 0.001, step = 0.005, stop = 30))
+#xw.write_col("freq/cm^-1", freq_cm)
 freq_hz = 3*10**10/freq_cm
-xw.write_col("freq/Hz", freq_hz)
+#xw.write_col("freq/Hz", freq_hz)
 freq_thz = freq_hz*10**(-12)
-xw.write_col("freq/THz", freq_thz)
-f = SED(freq_thz)
-xw.write_col("Intensity", f)
-xw.save()
+#xw.write_col("freq/THz", freq_thz)
+f = SED(freq_hz)
+#xw.write_col("Intensity", f)
+#xw.save()
 
 plotter.loglogplot(freq_thz, f)
