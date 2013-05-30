6432c90bfc50262a1d65deac750e3be4c4dfac61
loisshen <loisshendream@gmail.com>
2013-05-30 13:28:18 -0600

    Update file_refs.py

--- file_refs.py
+++ file_refs.py
@@ -25,40 +25,26 @@ atm_rad_refs = {'30KmBalloon': "data/Backgrounds/Atmospheric Radiance_sites/30Km
                 }
 test = atm_rad_refs['30KmBalloon']
 
-'''./data/Backgrounds/Atmospheric Radiance_sites:
-30KmBalloon-Radiance-1976Model-45Deg-0-2000cm.xlsx
-40KmBalloon-Radiance-1976Model-45Deg-0-2000cm.xlsx
-CCAT-0732g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-CCAT-0978g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-DomeA-014g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-DomeA-01g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-DomeC-015g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-DomeC-024g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-MaunaKea-15g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-MaunaKea-1g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-SouthPole-023g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-SouthPole-032g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-WhiteMountain-115g-Radiance-1976Model-45Deg-0-2000cm.xlsx
-WhiteMountain-175g-Radiance-1976Model-45Deg-0-2000cm.xlsx
+from excel import ExcelReader
+atm_tran_refs = {'13_7Km SOFIA': "data/Backgrounds/Atmospheric Tranimission_sites/13_7Km SOFIA-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                 '30KmBalloon': "data/Backgrounds/Atmospheric Tranimission_sites/30KmBalloon-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                 '40KmBalloon': "data/Backgrounds/Atmospheric Tranimission_sites/40KmBalloon-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'CCAT-0732g': "data/Backgrounds/Atmospheric Tranimission_sites/CCAT-0732g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'CCAT-0978g': "data/Backgrounds/Atmospheric Tranimission_sites/CCAT-0978g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'DomeA-014g': "data/Backgrounds/Atmospheric Tranimission_sites/DomeA-014g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'DomeA-01g': "data/Backgrounds/Atmospheric Tranimission_sites/DomeA-01g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'DomeC-015g': "data/Backgrounds/Atmospheric Tranimission_sites/DomeC-015g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'DomeC-024g':"data/Backgrounds/Atmospheric Tranimission_sites/DomeC-024g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'MaunaKea-15g': "data/Backgrounds/Atmospheric Tranimission_sites/MaunaKea-15g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'MaunaKea-1g':"data/Backgrounds/Atmospheric Tranimission_sites/MaunaKea-1g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'SantaBarbara-01g': "data/Backgrounds/Atmospheric Tranimission_sites/SantaBarbara-01g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'SantaBarbara-3_0g':"data/Backgrounds/Atmospheric Tranimission_sites/SantaBarbara-3_0g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'SouthPole-023g': "data/Backgrounds/Atmospheric Tranimission_sites/SouthPole-023g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'SouthPole-032g': "data/Backgrounds/Atmospheric Tranimission_sites/SouthPole-032g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'WhiteMountain-115g': "data/Backgrounds/Atmospheric Tranimission_sites/WhiteMountain-115g-Transmission-1976Model-45Deg-0-2000cm.xlsx",
+                'WhiteMountain-175g':"data/Backgrounds/Atmospheric Tranimission_sites/WhiteMountain-175g-Transmission-1976Model-45Deg-0-2000cm.xlsx"
+                }
 
-./data/Backgrounds/Atmospheric Tranimission_sites:
-13_7Km SOFIA-Transmission-1976Model-45Deg-0-2000cm.xlsx
-30KmBalloon-Transmission-1976Model-45Deg-0-2000cm.xlsx
-40KmBalloon-Transmission-1976Model-45Deg-0-2000cm.xlsx
-CCAT-0732g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-CCAT-0978g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-DomeA-014g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-DomeA-01g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-DomeC-015g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-DomeC-024g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-MaunaKea-15g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-MaunaKea-1g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-SantaBarbara-01g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-SantaBarbara-3_0g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-SouthPole-023g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-SouthPole-032g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-WhiteMountain-115g-Transmission-1976Model-45Deg-0-2000cm.xlsx
-WhiteMountain-175g-Transmission-1976Model-45Deg-0-2000cm.xlsx
 
 ./data/Backgrounds/Galactic Emission:
 Galactic Emission_glat=0 glong=0.xlsx
