1ff5dd9837476dcbcabf828832784a8f99023ac7
Guan Yilun <zoom.aaron@gmail.com>
2013-06-11 15:32:07 -0700

    'update'

---
+++ Git_update.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def run_command(command):
+    p = subprocess.Popen(command, shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+    return p.communicate()
+
+result = run_command('git commit -a -m \'update\'')
+ret = result[0]
+
+if 'git add' in result[0]:
+    print 'in'
+    result = run_command('git add *.py')
+
+print result
