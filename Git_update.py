b84b798bba918a6512f1aa354d7e5ac23971234f
Guan Yilun <zoom.aaron@gmail.com>
2013-06-11 15:37:01 -0700

    'update'

--- Git_update.py
+++ Git_update.py
@@ -6,13 +6,19 @@ def run_command(command):
             stderr=subprocess.STDOUT)
     return p.communicate()
 
+print "Committing changes..."
 result = run_command('git commit -a -m \'update\'')
 ret = result[0]
+print "Commit DONE"
 
 if 'git add' in ret:
-    print 'in'
+    print 'Untracked files found, adding...'
     run_command('git add *.py')
+    print 'Untracked files added DONE'
+    print "Committing changes..."
     result = run_command('git commit -a -m \'update\'')
+    print "Commit DONE"
     ret = result[0]
     
+print "RESULT:"
 print ret
