2eb645d5d42255242d3c7de2058e9acaba733820
Quincy Cheung <quinister16@gmail.com>
2013-08-19 15:08:48 -0700

    changes to BLINGs(particularly CMB)
    for CMB, I have commented out some of the math and added calculations that look more like the equation but the plot is wrong so this could have been a mistake.  In these changes, I assumed v0 is the exactly the frequency used in the paper's equation.  I'm not sure where the "(3e10)/((2 * resol * 0.1))" used to calculate i_a and i_b come from so that could be the issue.

--- cal.py
+++ cal.py
@@ -5,14 +5,10 @@ from excel import ExcelReader
 
 #bling_sub is for: CIB, Galactic Emission, and Zodiacal Emission
 def bling_sub(freq, temp, resol):
-    freq = np.array(freq, dtype="float")
-    temp = np.array(temp, dtype="float")
-    print "freq =", freq
-    print "temp =", temp
     resol = float(resol)
     f = interpolate.InterpolatedUnivariateSpline(freq, temp, k=1)
     step_size = 1.5e5
-    c = const.h * const.k * step_size #increasing this step size shifts up the values
+    c = 2 * const.h * const.k * step_size 
     int_range_length = freq/2/resol
     int_range = np.zeros((len(freq), 2))
     int_range[:,0]=freq - int_range_length
@@ -23,40 +19,29 @@ def bling_sub(freq, temp, resol):
     result = np.array([c*np.sum(i*f(i)) for i in ranges])
     print "result is ", result
     return result
-   
-    
+    # POSSIBLE ISSUES: the mode=2, step size, data files
 
-#bling_Cosmic_Microwave_Background
+#bling_Cosmic Microwave Background
 def bling_CMB(freq, resol):
     result = []
+    freq = np.array(freq, dtype='float')
     for i, v in enumerate(freq):
-        i_a = int( i - v / (3e10)/((2 * resol * 0.1))) #question: why do we want this to be integer?
+        i_a = int(i - v / (3e10)/((2 * resol * 0.1)))
         i_b = int(i + v / (3e10)/ ((2 * resol * 0.1)))
         if i_a <= 0:
             i_a = 0
         if i_b > len(freq):
             i_b = len(freq)
 
-        v0 = freq[i_a:i_b]*int(const.h) #h shouldn't have to be made an integer
-        v0_1 = np.array(v0) #create array to square list
-        v0_2 = v0_1**2/np.exp(v0_1/const.k/const.T - 1)
-        result.append(6e9*np.sum(v0_2))
-    return np.array(result)
-  
-#bling_Thermal Mirror Emission
-def bling_TME(freq, resol, sigma, mirror_temp):
-    result = []
-    for i in range(len(freq)):
-        v = freq[i]
-        offset = v/(3*10**10*2*resol*0.1)
-        i_a = int(i - offset) if i >= offset else 0
-        i_b = int(i + offset) if len(freq) - offset >= i else len(freq)
-        v0 = freq[i_a:i_b]
-        v0 = v0**2.5/np.exp(const.h*v0/const.k/mirrot_temp - 1)
-        result.append(24e9*const.h**2*(np.pi*const.epsilon/sigma)**0.5*np.sum(v0))
+        v0 = np.array(freq[i_a:i_b])
+##        values = const.h*6e9*np.sum(v0**2/np.exp(v0/const.k/const.T - 1))
+        denom = np.exp((const.h * np.sum(v0))/(const.k * const.T)) - 1
+        values = 2 * const.h * (np.sum(v0 ** 3))/(const.c ** 2)/denom
+        result.append(values)
+    print np.array(result)
     return np.array(result)
     
-#bling_Atmospheric_Radiance
+#bling_Atmospheric Radiance
 def bling_AR(freq, rad, resol):
     result = []
     for i, v in enumerate(freq):
@@ -70,6 +55,19 @@ def bling_AR(freq, rad, resol):
         freq_array = np.array(freq[i_a:i_b])
         t0 = rad_array/freq_array #divide arrays instead of lists
         result.append(const.c**2*const.h*3e9*np.sum(t0))
+    return np.array(result)
+
+#bling_Thermal Mirror Emission
+def bling_TME(freq, resol, sigma, mirror_temp):
+    result = []
+    for i in range(len(freq)):
+        v = freq[i]
+        offset = v/(3*10**10*2*resol*0.1)
+        i_a = int(i - offset) if i >= offset else 0
+        i_b = int(i + offset) if len(freq) - offset >= i else len(freq)
+        v0 = np.array(freq[i_a:i_b])
+        v0 = v0**2.5/np.exp(const.h*v0/const.k/mirror_temp - 1)
+        result.append(24e9*const.h**2*(np.pi*const.epsilon/sigma)**0.5*np.sum(v0))
     return np.array(result)
 
 #Limiting_Flux
