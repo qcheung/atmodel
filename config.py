5844b119d534b8a3906903d05886d71e0c902c7f
caglow <caglow@users.sourceforge.net>
2014-01-24 19:40:36 -0700

    Added a configuration file to allow some input fields to have default values.

---
+++ config.py
@@ -0,0 +1,12 @@
+# atmodel: config.py
+# configurable settings
+# (including default input values)
+
+SpecRes_Default = ""     # Spectral Resolution
+MirrorDiam_Default = ""  # Mirror Diameter (m)
+MirrorTemp_Default = ""  # Mirror Temperature (K)
+StartFreq_Default = ""   # Starting Frequency (THz)
+StopFreq_Default = ""    # Ending Frequency (THz)
+SigNoise_Default = ""    # Signal to Noise Ratio
+DependMin_Default = ""   # Dependent Minimum (10^x)
+DependMax_Default = ""   # Dependent Maximum (10^x)
